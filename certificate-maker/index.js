481f14fb2f11f91f09fc6148dbe2886783981a5f
Kapil Sonone <ksonone8@gmail.com>
2022-11-21 12:21:17 +0530

    Update index.js

--- certificate-maker/index.js
+++ certificate-maker/index.js
@@ -20,8 +20,8 @@ const generatePDF = async (name)=>{
     const firstPage = pages[0];
 
     firstPage.drawText(name, {
-        x: 550,
-        y: 500,
+        x: 450,
+        y: 480,
         size: 48,
         font: myFont,
         color: rgb(1,0.83,0.24),
